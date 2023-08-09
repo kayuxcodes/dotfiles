7af347d1f60dd5ac9b94181857da81d79239abf0
Zohir Benghalem <kayuxcodes@gmail.com>
2023-08-09 20:30:51 +0100

    ability to change win theme, reload config

--- pwsh/profile.ps1
+++ pwsh/profile.ps1
@@ -3,18 +3,36 @@ param (
 )
 # options-for-configuration--------------------------------------------------------------------------------------------
 $windowsTerminalSettingsPath = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_*\LocalState\settings.json"
+$fzfSettings = "
+--pointer='∩äà'
+--marker='∩äà'
+--prompt='∩üö '
+--border='sharp'
+"
 # dark
-$darkColorschemeName = "Poimandres"
+$darkColorSchemeName = "Poimandres"
 $darkBackgroundImagePath = "$Home\Pictures\dark.jpg"
 $darkOhMyPosh = "$Home/Documents/Powershell/poimandres.omp.json" 
+
+$darkFzf = $fzfSettings + @"
+          --color=bg+:#303340,gutter:-1,spinner:#6DE4C7,hl:#5DE4C7,border:#7390AA
+          --color=fg:#A6ACCD,header:#FF23FB,info:#FFFAC2,pointer:#89DDFF
+          --color=marker:#FFFAC2,fg+:#E4F0FB,prompt:#D0679D,hl+:#D0679D
+"@
 # light
 $lightBackgroundImagePath = "$Home\Pictures\light.jpg"
-$lightColorschemeName = "Catppuccin Latte"
-$lightOhMyPosh = "$Home/Documents/Powereshell/kayux.omp.json" 
+$lightColorSchemeName = "Catppuccin Latte"
+$lightOhMyPosh = "$Home/Documents/Powershell/kayux.omp.json" 
+$lightFzf = $fzfSettings + @"
+--color=bg+:#ccd0da,gutter:-1,spinner:#dc8a78,hl:#d20f39,border:#eff1f5
+--color=fg:#4c4f69,header:#d20f39,info:#8839ef,pointer:#dc8a78
+--color=marker:#dc8a78,fg+:#4c4f69,prompt:#8839ef,hl+:#d20f39
+"@
 # jsonContent for Windows Terminal json settings
 $wtSettings = Get-Content -Path $windowsTerminalSettingsPath -Raw | ConvertFrom-Json
 # this function will call after the settings will applied
 function Set-Envs {
+  $wtProfile = $windowsTerminalSettingsPath
   $defaults = $wtSettings.profiles.defaults
   $env:ThemeName=$defaults.colorScheme
   $env:TerminalFont=$defaults.font.face
@@ -23,22 +41,39 @@ function Set-Envs {
   $nfProfile = $env:NEOFETCH_PROFILE 
 }
 # ---------------------------------------------------------------------------------------------------------------------
-
 function Get-WindowsDarkThemeStatus {
     $registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize"
     if (Test-Path $registryPath) {
         $themeProperties = Get-ItemProperty -Path $registryPath
         # Check if the "AppsUseLightTheme" registry value is set to 0 (dark theme) or 1 (light theme)
-        $isDarkThemeEnabled = $themeProperties.AppsUseLightTheme -eq 0
+       $isDarkThemeEnabled = $themeProperties.AppsUseLightTheme -eq 0
         return $isDarkThemeEnabled
     } else {
         Write-Host "Theme information not found. Are you using a supported version of Windows?"
         return $false
     }
 }
+function Set-WindowsTheme {
+    param (
+        [Parameter(Mandatory=$true)]
+        [ValidateSet('dark','light')]
+        [string]$mode
+    )
+      $UseDarkTheme = $mode -eq 'dark'? $true : $false
+      $isDarkThemeEnabled = Get-WindowsDarkThemeStatus
+      if (($isDarkThemeEnabled -ne $UseDarkTheme) -or (!$isDarkThemeEnabled -ne !$UseDarkTheme)) {
+        $themeKeyPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize'
+          $themeValue = $UseDarkTheme ? 0 : 1
+          $systemThemeValue = $UseDarkTheme ? 0 : 1
+          Set-ItemProperty -Path $themeKeyPath -Name "AppsUseLightTheme" -Value $themeValue
+          Set-ItemProperty -Path $themeKeyPath -Name "SystemUsesLightTheme" -Value $systemThemeValue
+      } else {
+        Write-Host "$mode theme is already set"
+      }
+}
 function Set-WallPaper {
   param (
-      [parameter(Mandatory=$True)]
+      [parameter(Mandatory=$true)]
       [string]$Image
       )
     Add-Type -TypeDefinition @" 
@@ -53,30 +88,30 @@ function Set-WallPaper {
     $fWinIni = 0x01 -bor 0x02
     $ret = [Params]::SystemParametersInfo(0x0014, 0, $Image, $fWinIni)
 }
-function Set-WindowsTerminalColorScheme {
+function Set-Config {
   if (Test-Path $windowsTerminalSettingsPath) {
     $isDarkThemeEnabled = Get-WindowsDarkThemeStatus
-    $selectedColorscheme = if ($isDarkThemeEnabled) {$darkColorschemeName} else { $lightColorschemeName }
+    $selectedColorScheme = $isDarkThemeEnabled ? $darkColorSchemeName : $lightColorSchemeName
+    $currentBgPath = (Get-ItemProperty -Path 'HKCU:\Control Panel\Desktop\' -Name 'Wallpaper').Wallpaper
+    $currentBgName = Split-Path -Path $currentBgPath -Leaf
+    # set fzf theme
+    $env:FZF_DEFAULT_OPTS=($isDarkThemeEnabled ? $darkFzf : $lightFzf)
     # Ensure the 'profiles' and 'defaults' properties exist in the JSON object
-    if (-not $wtSettings.profiles) {
-      $wtSettings.profiles = @{}
-    }
-    if (-not $wtSettings.profiles.defaults) {
-      $wtSettings.profiles.defaults = @{}
-    }
-    $wtSettings.profiles.defaults.colorScheme = $selectedColorscheme
+    if (-not $wtSettings.profiles) {$wtSettings.profiles = @{}}
+    if (-not $wtSettings.profiles.defaults) {$wtSettings.profiles.defaults = @{}}
+    $wtSettings.profiles.defaults.colorScheme = $selectedColorScheme
     $updatedJsonContent = $wtSettings | ConvertTo-Json -Depth 4
     $updatedJsonContent | Set-Content -Path $windowsTerminalSettingsPath -Encoding UTF8
-    # Change the Windows background based on the theme
-      $currentBgPath = (Get-ItemProperty -Path 'HKCU:\Control Panel\Desktop\' -Name 'Wallpaper').Wallpaper
-      $currentBgName = "'$(Split-Path -Path $currentBgPath -Leaf)'"
+    # Change the Windows background,ohmyposh based on theme
       if ($isDarkThemeEnabled) {
+        oh-my-posh init pwsh --config $darkOhMyPosh | Invoke-Expression
         if ($currentBgPath -ne $darkBackgroundImagePath) {
           Set-WallPaper -Image $darkBackgroundImagePath
         } else {
           Write-Host("$currentBgName Background is already set")
         }
       } else {
+        oh-my-posh init pwsh --config $lightOhMyPosh | Invoke-Expression
         if ($currentBgPath -ne $lightBackgroundImagePath) {
           Set-WallPaper -Image $lightBackgroundImagePath
         } else {
@@ -87,11 +122,14 @@ function Set-WindowsTerminalColorScheme {
     Write-Host "Windows Terminal settings.json not found. Make sure Windows Terminal is installed and run it at least once."
   }
 }
-# settings up oh-my-posh config based on the theme
-if (Get-WindowsDarkThemeStatus) {
-  oh-my-posh init pwsh --config $darkOhMyPosh | Invoke-Expression
-} else {
-  oh-my-posh init pwsh --config $lightOhMyPosh | Invoke-Expression
+function changeTheme {
+  param (
+      [Parameter(Mandatory=$true)]
+      [ValidateSet('dark','light')]
+      [string]$Theme
+      )
+        Set-WindowsTheme -mode $Theme
+        Set-Config
 }
 # PSRadLine Options
 Set-PSReadLineOption -PredictionSource History
@@ -107,7 +145,18 @@ Set-PSReadLineKeyHandler -Key "Ctrl+l" -Function ForwardWord
 # Modules
 Import-Module -name Terminal-Icons
 # Aliases
-Set-Alias vim nvim
+function vim {
+  param (
+      # find files using `fzf`
+      [string]$path,
+      [switch]$f
+      ) 
+    if (-not $f) {
+      nvim $path
+    } else {
+      nvim $(fzf)
+    }  
+}
 Set-Alias pt "~\scoop\apps\powertoys\0.71.0\PowerToys.exe"
 # Utility Functions
 # delete files and folders
@@ -179,7 +228,7 @@ function cme($path) {
   }
 }
 if (-not $lazy) {
-  Set-WindowsTerminalColorScheme
+  Set-Config
   Set-Envs
   if (Get-Command "neofetch" -ErrorAction SilentlyContinue) {
     neofetch
